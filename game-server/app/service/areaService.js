d41786b3f8de8ae7b1047b0e9cbba316ad233a31
jianan <jianan@happyplaygame.net>
2018-08-21 16:40:44 +0800

    地图服务

--- game-server/app/service/areaService.js
+++ game-server/app/service/areaService.js
@@ -85,14 +85,16 @@ AreaService.prototype.addEvent = function (player) {
   //
   var self = this;
 
-  //玩家发起宝物
+  //玩家捡起宝物
   player.on('pickItem', function (args) {
 
     //
     var player = self.getEntity(args.entityId);
 
-    //
+    //宝物
     var treasure = self.getEntity(args.target);
+
+
     player.target = null;
 
     //捡到宝物
@@ -118,13 +120,19 @@ AreaService.prototype.addEvent = function (player) {
 }
 
 AreaService.prototype.entityUpdate = function () {
+
+  //向客户端广播去掉一帧内应该去掉的实体对象
   if (this.reduced.length > 0) {
     this.getChannel().pushMessage({
       route: 'removeEntities',
       entities: this.reduced
     });
+
+    //
     this.reduced = [];
   }
+
+  //
   if (this.added.length > 0) {
     var added = this.added;
     var r = [];
@@ -132,10 +140,12 @@ AreaService.prototype.entityUpdate = function () {
       r.push(added[i].toJSON());
     }
 
+    //广播新增的实体对象
     this.getChannel().pushMessage({
       route: 'addEntities',
       entities: r
     });
+
     this.added = [];
   }
 };
@@ -148,25 +158,40 @@ AreaService.prototype.addEntity = function (e) {
     return false;
   }
 
+  //key：实体id   value：具体的对象
   this.entities[e.entityId] = e;
 
+  //判断，如果增加的是玩家的话
   if (e.type === this.consts.EntityType.PLAYER) {
+
+    //channel里面增加一个玩家，参数是：玩家id + 服务器id
     this.getChannel().add(e.id, e.serverId);
+
+    //
     this.addEvent(e);
 
+    //同一个玩家添加了2次，报错提醒
     if (!!this.players[e.id]) {
       logger.error('add player twice! player : %j', e);
     }
+
+    //玩家数组中记录下这个玩家实体id
     this.players[e.id] = e.entityId;
-  } else if (e.type === this.consts.EntityType.TREASURE) {
+  } 
+  //添加的是宝物的话，地图上宝物数量+1
+  else if (e.type === this.consts.EntityType.TREASURE) {
     this.treasureCount++;
   }
 
+  //不加区分的数组里面记录下这个实体
   this.added.push(e);
+
   return true;
 };
 
-
+/**
+ * 排名
+ */
 AreaService.prototype.rankUpdate = function () {
 
   //时钟累计
@@ -211,18 +236,29 @@ AreaService.prototype.removeEntity = function (entityId) {
   }
 
   if (e.type === this.consts.EntityType.PLAYER) {
+
+    //玩家离线了，则让玩家离开通道
     this.getChannel().leave(e.id, e.serverId);
+
+    //终止掉玩家的动作
     this.actionManagerService.abortAllAction(entityId);
 
+    //玩家离开了，玩家map中删除玩家信息
     delete this.players[e.id];
+
   } else if (e.type === this.consts.EntityType.TREASURE) {
+
     this.treasureCount--;
+
+    //宝物少于25个，立马再生成15个
     if (this.treasureCount < 25) {
       this.generateTreasures(15);
     }
   }
 
   delete this.entities[entityId];
+
+
   this.reduced.push(entityId);
   return true;
 };
@@ -236,6 +272,7 @@ AreaService.prototype.getEntity = function (entityId) {
 };
 
 /**
+ * id列表转化为实体列表
  * Get entities by given id list
  * @param {Array} The given entities' list.
  */
@@ -251,6 +288,7 @@ AreaService.prototype.getEntities = function (ids) {
   return result;
 };
 
+//玩家也属于实体的一部分.根据玩家id，得到实体列表
 AreaService.prototype.getAllPlayers = function () {
   var _players = [];
   var players = this.players;
@@ -261,12 +299,16 @@ AreaService.prototype.getAllPlayers = function () {
   return _players;
 };
 
+//生成n个宝物
 AreaService.prototype.generateTreasures = function (n) {
   if (!n) {
     return;
   }
+
   for (var i = 0; i < n; i++) {
     var d = this.dataApiUtil.treasure().random();
+
+    //可以看出来，生成的宝物，不包含坐标信息
     var t = bearcat.getBean('treasure', {
       kindId: d.id,
       kindName: d.name,
@@ -278,6 +320,9 @@ AreaService.prototype.generateTreasures = function (n) {
   }
 };
 
+/**
+ * 实体数据
+ */
 AreaService.prototype.getAllEntities = function () {
   var r = {};
   var entities = this.entities;
@@ -290,11 +335,13 @@ AreaService.prototype.getAllEntities = function () {
   // return this.entities;
 };
 
+//通过playerId得到玩家实体
 AreaService.prototype.getPlayer = function (playerId) {
   var entityId = this.players[playerId];
   return this.entities[entityId];
 };
 
+//移除一个玩家
 AreaService.prototype.removePlayer = function (playerId) {
   var entityId = this.players[playerId];
 
@@ -305,6 +352,7 @@ AreaService.prototype.removePlayer = function (playerId) {
 };
 
 /**
+ * 得到地图信息
  * Get area entities for given postion and range.
  */
 AreaService.prototype.getAreaInfo = function () {
