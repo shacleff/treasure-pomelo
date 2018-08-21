f72fe16dde11a6ea2d482fc3cb360ae669c6955a
jianan <jianan@happyplaygame.net>
2018-08-21 18:05:18 +0800

    注释

--- game-server/app/domain/action/move.js
+++ game-server/app/domain/action/move.js
@@ -16,24 +16,43 @@ var Move = function(opts) {
 
 Move.prototype.init = function() {
   var Action = bearcat.getFunction('action');
+
+  //相当于简单继承，把父类属性拿过来
   Action.call(this, this.opts);
 }
 
+//更新一次.
+//到底移动是
 Move.prototype.update = function() {
+  //移动了多长时间(当前时间 - 初始化这个动作时的时间)
   var time = Date.now() - this.time;
+
+  //实体移动速度
   var speed = this.entity.walkSpeed;
+
+  //移动长度等于 速度 * 时间(s)
   var moveLength = speed * time / 1000;
+
+  //实体当前位置 和 重点位置差
   var dis = getDis(this.entity.getPos(), this.endPos);
+
+  //
   if (dis <= moveLength / 2) {
+
     this.finished = true;
     this.entity.setPos(this.endPos.x, this.endPos.y);
     return;
+
   } else if (dis < 55 && this.entity.target) {
+
+    //距离目的距离很小，则捡到了宝物
     this.entity.emit('pickItem', {
       entityId: this.entity.entityId,
       target: this.entity.target
     });
   }
+
+  //
   var curPos = getPos(this.entity.getPos(), this.endPos, moveLength, dis);
   this.entity.setPos(curPos.x, curPos.y);
 
--- game-server/app/service/actionManagerService.js
+++ game-server/app/service/actionManagerService.js
@@ -2,11 +2,15 @@ var logger = require('pomelo-logger').getLogger('bearcat-treasures', 'ActionMana
 var Queue = require('pomelo-collection').queue;
 
 /**
+ * 动作服务
  * Action Manager, which is used to contrll all action
  */
 var ActionManagerService = function(opts) {
+
+	//
 	opts = opts || {};
 
+	//队列中最大元素个数
 	this.limit = opts.limit || 10000;
 
 	//The map used to abort or cancel action, it's a two level map, first leven key is type, second leven is id
@@ -21,14 +25,19 @@ var ActionManagerService = function(opts) {
  * @param {Object} action  The action to add, the order will be preserved
  */
 ActionManagerService.prototype.addAction = function(action) {
+
+	//只能被添加一次的动作，先暂停掉动作
 	if (action.singleton) {
 		this.abortAction(action.type, action.id);
 	}
 
+	//当前动作map
 	this.actionMap[action.type] = this.actionMap[action.type] || {};
 
+	//动作map里面添加另外一个动作
 	this.actionMap[action.type][action.id] = action;
 
+	//动作队列加入动作
 	return this.actionQueue.push(action);
 };
 
@@ -38,6 +47,7 @@ ActionManagerService.prototype.addAction = function(action) {
  * @param {String} id The action id
  */
 ActionManagerService.prototype.abortAction = function(type, id) {
+	//动作map没有这个动作，那就没办法停止
 	if (!this.actionMap[type] || !this.actionMap[type][id]) {
 		return;
 	}
@@ -47,6 +57,7 @@ ActionManagerService.prototype.abortAction = function(type, id) {
 };
 
 /**
+ * 停止某个id的动作
  * Abort all action by given id, it will find all action type
  */
 ActionManagerService.prototype.abortAllAction = function(id) {
@@ -70,10 +81,15 @@ ActionManagerService.prototype.update = function() {
 			continue;
 		}
 
+		//动作更新一下，接下来判断是否删除，还是冲洗放回队列
 		action.update();
+
+		//没有播放完毕，就重新放入队列末端
 		if (!action.finished) {
 			this.actionQueue.push(action);
-		} else {
+		} 
+		//播放完毕就删除
+		else {
 			delete this.actionMap[action.type][action.id];
 		}
 	}
